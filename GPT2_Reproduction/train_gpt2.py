0a0209320d74682655981fe1299430aebabaf80c
Rohit Krishnan <krishr.somu@gmail.com>
2024-09-22 21:01:44 +0530

    improved adamW betas, added global norm clipping

--- GPT2_Reproduction/train_gpt2.py
+++ GPT2_Reproduction/train_gpt2.py
@@ -235,7 +235,7 @@ model.to('cuda')
 model = torch.compile(model)
 
 # optimization
-optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
 for i in range(50):
     t0 = time.time()
     x, y = train_loader.next_batch()
@@ -244,12 +244,13 @@ for i in range(50):
     with torch.autocast(device_type='cuda', dtype=torch.bfloat16):
         logits, loss = model(x, y)
     loss.backward()
+    norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
     optimizer.step()
     torch.cuda.synchronize()
     t1 = time.time()
     dt = (t1 - t0) * 1000 # time diff in ms
     tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
-    print(f"step {i}, loss: {loss.item()}, dt: {dt:.2f}ms, tok/sec: {tokens_per_sec:.2f}")
+    print(f"step {i} | loss: {loss.item()} | norm: {norm:.4f} | dt: {dt:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
 
 
 
