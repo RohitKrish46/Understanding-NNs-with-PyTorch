dfd1c6f9cb12c3cd9df3c69184642e600adb2db8
Rohit Krishnan <krishr.somu@gmail.com>
2024-09-23 17:38:23 +0530

    Modified gitignore, train_gpt2.py ; Added hellaswag.py,

--- GPT2_Reproduction/train_gpt2.py
+++ GPT2_Reproduction/train_gpt2.py
@@ -1,3 +1,4 @@
+from hellaswag import render_example, iterate_examples
 import os
 import sys
 import math
@@ -14,8 +15,6 @@ from torch.nn import functional as F
 warnings.filterwarnings("ignore")
 torch._dynamo.config.suppress_errors = True
 
-
-device = 'cuda'
 class CausalSelfAttention(nn.Module):
 
     def __init__(self, config):
@@ -47,10 +46,6 @@ class CausalSelfAttention(nn.Module):
         y = self.c_proj(y)
         return y
     
-class TanhGELU(nn.Module):
-    def forward(self, input):
-        return 0.5 * input * (1.0 + torch.tanh(math.sqrt(2.0/math.pi) * (input + 0.044715 * torch.pow(input, 3.0))))
-
 
 class MLP(nn.Module):
 
@@ -142,54 +137,54 @@ class GPT(nn.Module):
         return logits, loss
 
 
-    @classmethod
-    def from_pretrained(cls, model_type):
-        """Loads pretrained GPT-2 model weights from huggingface"""
-        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
-        from transformers import GPT2LMHeadModel
-        print("loading weights from pretrained gpt: %s" % model_type)
+    # @classmethod
+    # def from_pretrained(cls, model_type):
+    #     """Loads pretrained GPT-2 model weights from huggingface"""
+    #     assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+    #     from transformers import GPT2LMHeadModel
+    #     print("loading weights from pretrained gpt: %s" % model_type)
 
-        # n_layer, n_head and n_embd are determined from model_type
-        config_args = {
-            'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
-            'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
-            'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
-            'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
-        }[model_type]
-        config_args['vocab_size'] = 50257 # always 50257 for GPT model checkpoints
-        config_args['block_size'] = 1024 # always 1024 for GPT model checkpoints
-        # create a from-scratch initialized minGPT model
-        config = GPTConfig(**config_args)
-        model = GPT(config)
-        sd = model.state_dict()
-        sd_keys = sd.keys()
-        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard this mask / buffer, not a param
+    #     # n_layer, n_head and n_embd are determined from model_type
+    #     config_args = {
+    #         'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+    #         'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
+    #         'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
+    #         'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
+    #     }[model_type]
+    #     config_args['vocab_size'] = 50257 # always 50257 for GPT model checkpoints
+    #     config_args['block_size'] = 1024 # always 1024 for GPT model checkpoints
+    #     # create a from-scratch initialized minGPT model
+    #     config = GPTConfig(**config_args)
+    #     model = GPT(config)
+    #     sd = model.state_dict()
+    #     sd_keys = sd.keys()
+    #     sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard this mask / buffer, not a param
 
-        # init a huggingface/transformers model
-        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
-        sd_hf = model_hf.state_dict()
+    #     # init a huggingface/transformers model
+    #     model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+    #     sd_hf = model_hf.state_dict()
 
-        # copy while ensuring all of the parameters are aligned and match in names and shapes
-        sd_keys_hf = sd_hf.keys()
-        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these, just a buffer
-        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # same, just the mask (buffer)
-        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
-        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
-        # this means that we have to transpose these weights when we import them
-        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
-        for k in sd_keys_hf:
-            if any(k.endswith(w) for w in transposed):
-                # special treatment for the Conv1D weights we need to transpose
-                assert sd_hf[k].shape[::-1] == sd[k].shape
-                with torch.no_grad():
-                    sd[k].copy_(sd_hf[k].t())
-            else:
-                # vanilla copy over the other parameters
-                assert sd_hf[k].shape == sd[k].shape
-                with torch.no_grad():
-                    sd[k].copy_(sd_hf[k])
+    #     # copy while ensuring all of the parameters are aligned and match in names and shapes
+    #     sd_keys_hf = sd_hf.keys()
+    #     sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these, just a buffer
+    #     sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # same, just the mask (buffer)
+    #     transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+    #     # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
+    #     # this means that we have to transpose these weights when we import them
+    #     assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+    #     for k in sd_keys_hf:
+    #         if any(k.endswith(w) for w in transposed):
+    #             # special treatment for the Conv1D weights we need to transpose
+    #             assert sd_hf[k].shape[::-1] == sd[k].shape
+    #             with torch.no_grad():
+    #                 sd[k].copy_(sd_hf[k].t())
+    #         else:
+    #             # vanilla copy over the other parameters
+    #             assert sd_hf[k].shape == sd[k].shape
+    #             with torch.no_grad():
+    #                 sd[k].copy_(sd_hf[k])
 
-        return model
+    #     return model
     
 
     def configure_optimizers(self, weight_decay, learning_rate, device):
@@ -220,8 +215,7 @@ class GPT(nn.Module):
 
 num_return_sequences = 5
 max_length = 30
-
-#model = GPT.from_pretrained('gpt2')
+device = 'cuda'
 
 def load_tokens(filename):
     npt = np.load(filename)
@@ -235,7 +229,7 @@ class DataLoaderLite:
         self.T = T
         assert split in {'train', 'val'}
 
-        # data root
+        # get shard file names
         data_root = "edu_fineweb10B"
         shards = os.listdir(data_root)
         shards = [s for s in shards if split in s]
@@ -264,7 +258,24 @@ class DataLoaderLite:
             self.current_position = B * T
         return x, y
 
-#device_type = "cuda" if device.startswith("cuda") else "cpu"
+def get_most_likely_row(tokens, mask, logits):
+    # evaluate the autoregressive loss at all positions
+    shift_logits = (logits[..., :-1, :]).contiguous()
+    shift_tokens = (tokens[..., 1:]).contiguous()
+    flat_shift_logits = shift_logits.view(-1, shift_logits.size(-1))
+    flat_shift_tokens = shift_tokens.view(-1)
+    shift_losses = F.cross_entropy(flat_shift_logits, flat_shift_tokens, reduction='none')
+    shift_losses = shift_losses.view(tokens.size(0), -1)
+    # now get the average loss just for the completion region (where mask == 1), in each row
+    shift_mask = (mask[..., 1:]).contiguous() # we must shift mask, so we start at the last prompt token
+    masked_shift_losses = shift_losses * shift_mask
+    # sum and divide by the number of 1s in the mask
+    sum_loss = masked_shift_losses.sum(dim=1)
+    avg_loss = sum_loss / shift_mask.sum(dim=1)
+    # now we have a loss for each of the 4 completions
+    # the one with the lowest loss should be the most likely
+    pred_norm = avg_loss.argmin().item()
+    return pred_norm
 
 torch.manual_seed(1337)
 if torch.cuda.is_available():
@@ -272,7 +283,7 @@ if torch.cuda.is_available():
 
 enc = tiktoken.get_encoding("gpt2")
 
-total_batch_size = 524288 # ~0.5 M, in number of tokens
+total_batch_size = 524288 # ~0.5 M, desired number of tokens to process
 B = 8 # mini batch size
 T = 1024 # sequence length
 assert total_batch_size % (B * T) == 0, "make sure total batch size is divisible by B * T"
@@ -281,8 +292,7 @@ print(f"total desired batch size : {total_batch_size}")
 print(f"=> calculated gradient accumulation steps: {grad_accum_steps}")
  
 
-
-train_loader = DataLoaderLite(B=8, T=1024, split='train')
+train_loader = DataLoaderLite(B=B, T=T, split='train')
 val_loader = DataLoaderLite(B=B, T=T, split='val')
 
 torch.set_float32_matmul_precision('high') #tensorfloat32 instead of float 32
@@ -290,11 +300,6 @@ torch.set_float32_matmul_precision('high') #tensorfloat32 instead of float 32
 model = GPT(GPTConfig(vocab_size=50304))
 model.to('cuda')
 #model = torch.compile(model)
-
-# max_lr = 6e-4
-# min_lr = max_lr * 0.1
-# warmup_steps = 10
-# max_steps = 50
 
 max_lr = 6e-4
 min_lr = max_lr * 0.1
@@ -317,13 +322,21 @@ def get_lr(it):
 
 
 # optimization
-
 optimizer = model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device="cuda")
+
+# create log dir for checkpoints logging (train loss, val loss, hellaswag accuracies)
+log_dir = "log"
+os.makedirs(log_dir, exist_ok=True)
+log_file = os.path.join(log_dir, "log.txt")
+with open(log_file, "w") as f:
+    pass
 
 for step in range(max_steps):
     t0 = time.time()
+    last_step = (step == max_steps - 1)
+
     # once in a while evaluate our validation loss
-    if step % 100 == 0:
+    if step % 250 == 0 or last_step:
         model.eval()
         val_loader.reset()
         with torch.no_grad():
@@ -338,9 +351,24 @@ for step in range(max_steps):
                 val_loss_accum += loss.detach()
 
         print(f"validation loss: {val_loss_accum.item():.4f}")
+        
+        with open(log_file, "a") as f:
+            f.write(f"{step} val {val_loss_accum.item():.4f}\n")
+        if step > 0 and (step % 5000 == 0 or last_step):
+            # optionally write model checkpoints
+            checkpoint_path = os.path.join(log_dir, f"model_{step:05d}.pt")
+            checkpoint = {
+                'model': model.state_dict(),
+                'config': model.config,
+                'step': step,
+                'val_loss': val_loss_accum.item()
+            }
+            # you might also want to add optimizer.state_dict() and
+            # rng seeds etc., if you wanted to more exactly resume training
+            torch.save(checkpoint, checkpoint_path)
 
     # once in a while generate from model
-    if step > 0 and step % 100 == 0:
+    if (step > 0 and step % 250 == 0) or last_step:
         model.eval()
         num_return_sequences = 4
         max_length = 32
@@ -373,6 +401,29 @@ for step in range(max_steps):
             decoded = enc.decode(tokens)
             print(f"sample {i}: {decoded}")
 
+    # once in a while evaluate hellaswag
+    if (step % 250 == 0 or last_step):
+        num_correct_norm = 0
+        num_total = 0
+        for i, example in enumerate(iterate_examples("val")):
+            # render the example into tokens and labels
+            _, tokens, mask, label = render_example(example)
+            tokens = tokens.to(device)
+            mask = mask.to(device)
+            # get the logits
+            with torch.no_grad():
+                with torch.autocast(device_type=device, dtype=torch.bfloat16):
+                    logits, loss = model(tokens)
+                pred_norm = get_most_likely_row(tokens, mask, logits)
+            num_total += 1
+            num_correct_norm += int(pred_norm == label)
+        acc_norm = num_correct_norm / num_total
+
+        print(f"HellaSwag accuracy: {num_correct_norm}/{num_total}={acc_norm:.4f}")
+        with open(log_file, "a") as f:
+            f.write(f"{step} hella {acc_norm:.4f}\n")
+
+    # train
     model.train()
     optimizer.zero_grad()
     loss_accum = 0.0
@@ -397,7 +448,5 @@ for step in range(max_steps):
     tokens_processed = train_loader.B * train_loader.T * grad_accum_steps
     tokens_per_sec = tokens_processed / dt
     print(f"step {step:4d} | loss: {loss_accum.item():.4f} | lr: {lr:.4e} | norm: {norm:.4f} | dt: {dt* 1000:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
-
-
-
-sys.exit(0)
+    with open(log_file, "a") as f:
+        f.write(f"{step} train {loss_accum.item():.6f}\n")
